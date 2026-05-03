337a26cfb467a870a7c38a528128795edd6b40ef
nico-slk1 <nicolas.selicki@gmail.com>
2026-05-02 23:06:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Welcome to React with Vite!</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
